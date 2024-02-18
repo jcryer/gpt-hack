19e3754794c9543a838a4f5aa43999aa265eb55b
Sam <sg2268@bath.ac.uk>
2024-02-18 19:31:03 +0000

    Started on some AI thiung

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ import * as path from 'path';
 import * as st from '@fastify/static';
 import { fileURLToPath } from 'url';
 
+import { processBankStatement } from './bank.js';
+import { parseInvoice } from './invoice.js';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -14,6 +17,13 @@ fastify.register(st, {
 
 fastify.get('/test', function(req, res) {
   return { hello: 'world' };
+});
+
+fastify.get('/do_it', async function(req, res) {
+  const bankFilePath = "data/CSV Jan 23.csv";
+  const bankData = await processBankStatement(bankFilePath);
+  const invFilePath = "data/Invoice TK01-38.pdf";
+  return await parseInvoice(invFilePath, bankData);
 });
 
 fastify.listen({ port: 3000 }, function (err, address) {
---
+++ backend/invoice.js
@@ -0,0 +1,37 @@
+import * as fs from "fs";
+import OpenAI from 'openai';
+import { pdfToBase64 } from './pdf.js';
+const openai = new OpenAI({
+    apiKey: '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'
+  });
+async function parseInvoice(filePath) {
+  const buffers = await pdfToBase64(filePath);
+  console.log("Bank Data\n:", JSON.stringify(bankData));
+  console.log(buffers[0]);
+  const pdfImg = buffers[0];
+
+  response = await openai.chat.completions.create({
+    model: "gpt-4-vision-preview",
+    messages: [
+      {
+        "role": "user",
+        "content": [
+          { "type": "text", "text": "This image is an invoice, find the most likely bank transaction that this invoice is associated with" },
+          {
+            "type": "image_url",
+            "image_url": {
+              "url": `data:image/jpeg;base64,${pdfImg}`,
+            }
+          }],
+      },
+      {
+        "role": "system",
+        "content": `You are an accounting bot. Here is some bank statement data in a json format. Use this information to match invoices to transactions. Data:\n${JSON.stringify(bankData)}`
+      }
+    ]
+  });
+  console.log(response.choices[0]);
+  return {data: response};
+}
+
+export { parseInvoice };
