ab0710a12689c9bff24b54a28d5381ea0a7cc4b4
Joseph Cryer <jcryer1234@gmail.com>
2024-02-19 17:52:14 +0000

    small embedding tests

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,8 @@ import * as path from 'path';
 import * as st from '@fastify/static';
 import { fileURLToPath } from 'url';
 import fs from "fs";
-import { processBankStatement, preprocessInvoice, processInvoice } from './openai.js';
-import { pdfToBase64, pdfToTextOcr, pdfToTextDirect } from './pdf.js';
+import { processBankStatement, processInvoice, testEmbedding } from './openai.js';
+import { pdfToText } from './pdf.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,60 +18,29 @@ fastify.register(st, {
 // Reads in a local CSV file of bank statement, chunks it, sends each chunk to GPT in parallel, collates result
 // and returns standardised JSON representation of bank statement.
 fastify.get('/bankStatement', async function(req, res) {
-  const bankFilePath = "data/CSV Jan 23.csv";
+  const bankFilePath = "data/monzo.csv";
   const csv = await fs.promises.readFile(bankFilePath, { encoding: "utf-8" });
   return JSON.stringify(await processBankStatement(csv), null, 2);
-});
-
-// Reads in a local PDF file of invoice, converts it to a base64 image, sends to GPT to describe it.
-fastify.get('/preInvoiceGPT', async function(req, res) {
-  const invoiceFilePath = "data/RS614063.pdf";
-  const buffer = await fs.promises.readFile(invoiceFilePath);
-  const images = await pdfToBase64(buffer);
-  return { data: await preprocessInvoice(images) };
 });
 
 // Reads in a local PDF file of invoice, converts it to an image, runs OCR to get all text.
 fastify.get('/preInvoiceOCR', async function(req, res) {
   const invoiceFilePath = "data/RS614063.pdf";
   const buffer = await fs.promises.readFile(invoiceFilePath);
-  return { data: await pdfToTextOcr(buffer) };
-});
-
-// Reads in a local PDF file of invoice, uses pdf.js to scrape all text.
-fastify.get('/preInvoiceText', async function(req, res) {
-  const invoiceFilePath = "data/RS614063.pdf";
-  const buffer = await fs.promises.readFile(invoiceFilePath);
-  return { data: await pdfToTextDirect(buffer) };
-});
-
-// Reads in a local PDF file of invoice, converts it to a base64 image, sends to GPT to describe it.
-// Then sends that description to GPT and returns standardised JSON representation of receipt.
-fastify.get('/invoiceGPT', async function(req, res) {
-  const invoiceFilePath = "data/coffee.pdf";
-  const buffer = await fs.promises.readFile(invoiceFilePath);
-  const images = await pdfToBase64(buffer);
-  const desc = await preprocessInvoice(images);
-  return JSON.stringify(await processInvoice(desc), null, 2);
-
+  return `${await pdfToText(buffer)}`;
 });
 
 // Reads in a local PDF file of invoice, converts it to an image, runs OCR to get all text.
 // Then sends that description to GPT and returns standardised JSON representation of receipt.
 fastify.get('/invoiceOCR', async function(req, res) {
-  const invoiceFilePath = "data/RS614063.pdf";
+  const invoiceFilePath = "data/215908-Telekinetix Limited.pdf";
   const buffer = await fs.promises.readFile(invoiceFilePath);
-  const desc = await pdfToTextOcr(buffer);
+  const desc = await pdfToText(buffer);
   return JSON.stringify(await processInvoice(desc), null, 2);
 });
 
-// Reads in a local PDF file of invoice, uses pdf.js to scrape all text.
-// Then sends that description to GPT and returns standardised JSON representation of receipt.
-fastify.get('/invoiceText', async function(req, res) {
-  const invoiceFilePath = "data/RS614063.pdf";
-  const buffer = await fs.promises.readFile(invoiceFilePath);
-  const desc = await pdfToTextDirect(buffer);
-  return JSON.stringify(await processInvoice(desc), null, 2);
+fastify.get('/test', async function(req, res) {
+  return JSON.stringify(await testEmbedding(), null, 2);
 });
 
 fastify.listen({ port: 3000 }, function (err, address) {
--- backend/pdf.js
+++ backend/pdf.js
@@ -1,5 +1,4 @@
 import * as pdfjs from 'pdfjs-dist';
-import * as fs from "fs";
 import { createWorker } from 'tesseract.js';
 import pdftopic from "pdftopic";
 
@@ -7,12 +6,7 @@ async function pdfToImageBuffers(fileBuffer) {
   return await pdftopic.pdftobuffer(fileBuffer, "all");
 }
 
-async function pdfToBase64(fileBuffer) {
-  const res = await pdfToImageBuffers(fileBuffer);
-  return res.map(x => x.toString('base64'));
-}
-
-async function pdfToTextOcr(fileBuffer) {
+async function pdfToText(fileBuffer) {
   const worker = await createWorker('eng');
   const images = await pdfToImageBuffers(fileBuffer);
   let out = "";
@@ -25,27 +19,4 @@ async function pdfToTextOcr(fileBuffer) {
   return out;
 }
 
-async function pdfToTextDirect(buffer) {
-  const pdf = await pdfjs.getDocument({data: buffer.buffer}).promise;
-  let out = "";
-
-  for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
-    const page = await pdf.getPage(pageNum);
-    const content = await page.getTextContent();
-    let textItems = content.items;
-    let line = 0;
-  
-    for (let i = 0; i < textItems.length; i++) {
-      if (line != textItems[i].transform[5]) {
-        if (line != 0) {
-          out +='\r\n';
-        }
-        line = textItems[i].transform[5];
-      }
-      out += textItems[i].str;
-    }
-  }
-  return out;
-}
-
-export { pdfToBase64, pdfToTextOcr, pdfToTextDirect };
+export { pdfToText };
